93dd787fea2af8d1229d62b35fcba325ab4ef7df
sabyasachi112345 <158844279+sabyasachi112345@users.noreply.github.com>
2024-03-01 12:59:29 +0530

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+<script>
+    if username==("CWKH326H")&&password==("Liverpoolfootball11");
+    console.log("log in successfully");
+    else{
+    console.log("wrong username and password")
+    }
+    </script>
